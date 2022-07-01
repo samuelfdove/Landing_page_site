98802a99f3515308f6df1e497ea822e347943210
gopancakes <samuelfdove@gmail.com>
2022-07-01 16:05:23 -0500

    changing header

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ firebase.initializeApp({
 function Header() {
   return(
     <Helmet>
-      <title>My Chess AI</title>
-      <meta name="description" content="This is the page to play my ChessAI" />
+      <title>Samuel Dove!</title>
+      <meta name="description" content="Hello" />
       
       <script src="jquery-3.6.0.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
